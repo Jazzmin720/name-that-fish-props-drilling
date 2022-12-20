1053dff314b0008fcef45478ce895591fb7a19a9
Jazzmin Smith <jazzmin.smith@centretechnologies.com>
2022-12-20 15:23:19 -0600

    set state with context api

--- src/Components/GameBoard.jsx
+++ src/Components/GameBoard.jsx
@@ -1,6 +1,9 @@
-import React, {useState,useEffect} from 'react';
+import React,{useContext,useEffect,useState} from 'react';
 import "./styles/game-board.css";
 import { Images } from "../assets/images";
+import {AppContext} from '../Context';
+//import {ContextProvider} from '../Context';
+
 
 const initialFishes = [
   {
@@ -23,21 +26,35 @@ const initialFishes = [
 
 
 
+
+
 export const GameBoard = (props) => {
   const nextFishToName = initialFishes[0];
+  const [state,setState] = useState("old state")
  
+ //state us currently being pulled from App.jsx provider
+  const useTheState = useContext(AppContext);
   
-  //set the state here
-  const [state, setState] = useState([]);
+ 
   //handle what happens when you submit the form
-   let handleChange = (e) => {
+   let handleSubmit = (e) => {
     e.preventDefault();
+    console.log({state})
+    
+  }
+
+  //change the state with each keypress
+  let changeTheState = (e)  => {
+    e.preventDefault();
+    setState(e.target.value)
     console.log(state)
   }
+
   
+   
   
     
- //onSubmit will fire the handleChange function in the form tag
+ //onSubmit will fire the handleSubmit function in the form tag
  //in the text input, onChange is fired after every key stroke, so the state is being changed to equal every key you hit
  //once you hit submit, whatever you had in the textbox at the time of submission becomes the new state
 
@@ -47,11 +64,12 @@ export const GameBoard = (props) => {
       <div id="fish-container">
         <img src={nextFishToName.url} alt={nextFishToName.name} />
       </div>
-      <form id="fish-guess-form" onSubmit={e => {handleChange(e)}} >
+      <form id="fish-guess-form" onSubmit={handleSubmit} >
         <label htmlFor="fish-guess">What kind of fish is this?</label>
-        <input type="text" name="fish-guess" value={state}  onChange={(e) => setState(e.target.value)} />
-        <input type="submit"   />
+        <input type="text" name="fish-guess" onChange = {changeTheState} />
         {console.log(state)}
+        <input type="submit"   />
+       
       </form>
     </div>
   );
